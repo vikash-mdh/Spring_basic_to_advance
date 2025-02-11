2ee442877428ceed8452c8c034127b7ef093bbf6
unknown <vikashkumar555550@gmail.com>
2025-02-11 09:49:57 +0530

    add secfile

--- src/main/java/org/example/Arrays_3/array_basic.java
+++ src/main/java/org/example/Arrays_3/array_basic.java
@@ -2,6 +2,6 @@ package org.example.Arrays_3;
 
 public class array_basic {
     public static void main(String[] args) {
-        System.out.println("hello vikash");g
+        System.out.println("hello vikash");
     }
 }
